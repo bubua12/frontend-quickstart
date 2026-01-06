4e3f1df5eff878bf686a47441333e68541964d1e
wangxl <wangty334752@gmail.com>
2026-01-06 19:26:41 +0800

    feat: 使用vite进行Vue项目初始化

---
+++ demo04-vue3/vue-demo/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hello from "./components/Hello.vue";
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <Hello></Hello>
+</template>
+
+<style scoped>
+</style>
---
+++ demo04-vue3/vue-demo/src/components/Hello.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h2>哈哈</h2>
+</template>
+
+<style scoped>
+
+</style>
